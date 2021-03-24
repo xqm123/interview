80e60f6c3f31ecb8163a46eee32e34df805f0536
ankerxiong <ankerxiong@muchenglin.com>
2021-03-24 19:59:39 +0800

    x

--- main.go
+++ main.go
@@ -15,21 +15,21 @@ func main() {
 	slice := make([]int, n)
 	slice2 := make([]int, n)
 	for i := 0; i < n; i++ {
-		slice[i] = rand.Intn(n)-n/2
+		slice[i] = rand.Intn(n) - n/2
 		slice2[i] = slice[i]
 	}
 
 	//fmt.Println(slice)
 
-	t1 := time.Now().UnixNano()/1e6
+	t1 := time.Now().UnixNano() / 1e6
 	sort.QuickSort(slice)
-	t2 := time.Now().UnixNano()/1e6
+	t2 := time.Now().UnixNano() / 1e6
 	//fmt.Println(slice)
 	fmt.Printf("QuickSort spend time: %d \n", t2-t1)
 
-	t1 = time.Now().UnixNano()/1e6
+	t1 = time.Now().UnixNano() / 1e6
 	sort.MergeSort(slice)
-	t2 = time.Now().UnixNano()/1e6
+	t2 = time.Now().UnixNano() / 1e6
 	//fmt.Println(slice)
 	fmt.Printf("MergeSort spend time: %d \n", t2-t1)
 
